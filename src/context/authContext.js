bf80f97998f8d9e54f66ab15d19f30905177e10c
Carrington <hartis.c99@gmail.com>
2024-05-26 11:38:42 -0400

    set default profile pic

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {createContext, useEffect, useState } from "react";
+import defaultPic from "../../upload/default-pic.jpg";
 
 export const AuthContext = createContext();
 
@@ -20,7 +21,12 @@ export const AuthContextProvider = ({ children }) => {
   }
 
   const register = async (input) => {
-    const res = await axios.post('https://virtual-vanguard-mmo-f84f119b0dd9.herokuapp.com/auth/register', input);
+    const setDefaultPic = {
+      ...input,
+      profilePic: defaultPic
+    };
+
+    const res = await axios.post('https://virtual-vanguard-mmo-f84f119b0dd9.herokuapp.com/auth/register', input, setDefaultPic);
     setCurrentUser(res.data);
   }
   
